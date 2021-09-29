c5daa496862e730d47e631b56d55a286d55048ca
p-6969 <64603999+p-6969@users.noreply.github.com>
2021-09-29 11:22:18 -0400

    uptd initial commit
    simple 2D platform

---
+++ Plataforma2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+}
